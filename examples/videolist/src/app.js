79fb9968552aaa3eb8867711dcacc65ee2a460ac
Jean-pierre Vincent <jpvirtual@yahoo.com>
2011-06-30 18:17:33 +0200

    autoplay first video depending on the connector

--- examples/videolist/src/app.js
+++ examples/videolist/src/app.js
@@ -11,23 +11,37 @@
 
 
 Joshfire.define(['joshfire/app', 'joshfire/class', './tree.data', './tree.ui', 'joshfire/vendor/underscore', 'joshfire/utils/splashscreen'],
-    function(App, Class, Data, UI, _, Splash) {
-      Joshfire.debug = true;
-
-      return Class(App, {
-        id: 'exampleVideoList',
-        uiClass: UI,
-        dataClass: Data,
-        setup: function(callback) {
-          var self = this,
-              splash = new Splash();
-          // Select first video on load
-          self.ui.element('/videolist').subscribe('data', _.once(function(ev, data) {
-            self.ui.setState('focus', '/videolist');
-            //remove splash
-            splash.remove();
-          }));
-          callback(null);
+function(App, Class, Data, UI, _, Splash) {
+  Joshfire.debug = true;
+  
+  // Behaviour specialization : some environments should not autoplay the video, as it is fullscreen
+  if(Joshfire.adapter === 'samsungtv'
+    || Joshfire.adapter === 'browser') {
+    var bAutoPlay = true;
+  } else {
+    var bAutoPlay = false;
+  }
+  //console.log( 'JOSHFIRE');
+  
+  return Class(App, {
+    id: 'exampleVideoList',
+    uiClass: UI,
+    dataClass: Data,
+    setup: function(callback) {
+      var self = this,
+          splash = new Splash();
+      // Select first video as soon we get the data
+      self.ui.element('/videolist').subscribe('data', _.once(function(ev, data) {
+        self.ui.setState('focus', '/videolist');
+        if(bAutoPlay === true) {
+          // retrieve the first element in the list and select it. 
+          // the onSelect method in tree.ui.js does the rest
+          self.ui.element('/videolist').selectByIndex(0);
         }
-      });
-    });
+        //remove splash
+        splash.remove();
+      }));
+      callback(null);
+    }
+  });
+});
