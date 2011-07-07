3fef0fe91d8c19a4208b63ecfc2aa70ee09d2e47
Thomas Bassetto <tbassetto@gmail.com>
2011-07-07 16:51:10 +0200

    Small fixes according to coding guidelines

--- test/general/datasource.js
+++ test/general/datasource.js
@@ -9,99 +9,52 @@
  * Date: Wed Jun 29 16:25:37 2011
  */
 
-
 var Q = QUnit;
 
 Q.config.autostart = false;
 Joshfire.debug = true;
 
-
-
 module('Utils / DataSource');
 
-
 Joshfire.define(['joshfire/utils/datasource'], function(DataSource) {
-
   test('general test', function() {
-
     expect(3);
-
     stop();
-
     var ds = new DataSource();
-
     ds.request({'url': 'http://www.whattimeisit.com/'},function(err,page1) {
-
       ok(page1.indexOf('Coordinated Universal Time') > 0);
-
       setTimeout(function() {
-
         ds.request({'url': 'http://www.whattimeisit.com/'},function(err,page2) {
-
           ok(page2.indexOf('Coordinated Universal Time') > 0);
-
           ok(page1 != page2);
-
           start();
-
         });
-
       },2000);
-
-
-
     });
-
   });
 
   test('cache test', function() {
-
     expect(5);
-
     stop();
-
     var ds = new DataSource();
-
     ds.request({'url': 'http://www.whattimeisit.com/', 'cache': 3},function(err,page1) {
-
       ok(page1.indexOf('Coordinated Universal Time') > 0);
-
       setTimeout(function() {
-
         ds.request({'url': 'http://www.whattimeisit.com/', 'cache': 3},function(err,page2) {
-
           ok(page2.indexOf('Coordinated Universal Time') > 0);
-
           ok(page1 == page2);
-
-
           setTimeout(function() {
-
             //Cache should now be expired
             ds.request({'url': 'http://www.whattimeisit.com/', 'cache': 3},function(err,page3) {
-
               ok(page1.indexOf('Coordinated Universal Time') > 0);
-
               ok(page1 != page3);
-
               start();
-
             });
-
           },2000);
-
-
         });
-
       },2000);
-
-
-
     });
-
   });
 
   start();
-
-
 });
