5405ac5691fe157df9c859676359522a1dc35f6b
Arthur Jamain <arthur.jamain@gmail.com>
2013-07-15 18:40:24 +0200

    Added commander support. This allows for CLI options...
    Which enables us to specify options in whichever order we want and be a little more explicit about it. Retro compatibility tests remain to be done for this version.
    
    Also added support for several new options which allow the user to specify a device, formfactor, etc. without having to specify a complete user agent.

--- scripts/optimize.js
+++ scripts/optimize.js
@@ -38,10 +38,52 @@ var exec = require('child_process').exec;
 var woodmanPrecompile = require('woodman/precompile/precompile');
 var UglifyJS = require('uglify-js');
 var UAParser = require('../lib/utils/uaparser/node/parse');
+var args = require('commander');
+
+/* Everything is optional for now. Compatibility. */
+args.version('0.1.0')
+    .option('-a, --adapter [adapter]'           , 'Required. The adapter which should be used.')
+    .option('-i, --input [input]'               , 'Required. The main file of the app.')
+    .option('-m, --minify [minify]'             , 'Whether to minify or not the outputted code')
+    .option('-w, --woodman [woodman]'           , 'Which levels of woodman should be kept after optimization. (Omit to remove them all)')
+    .option('-u, --useragent [useragent]'       , 'A specific user agent that matches the target device\'s. Used by the devicedetect plugin.')
+    .option('-d, --device [device]'             , 'The target device family (devicedetect) — i.e. : iPhone, Nexus 4...')
+    .option('-f, --formfactor [formfactor]'     , 'The target device form factor (devicedetect) — i.e. : phone, tablet ...')
+    .option('-s, --system [system]'             , 'The target device\'s OS — i.e. : Android, iPhone OS ...')
+    .option('--systemVersion [systemversion]'   , 'The target device\'s OS\'s version — e.g. : 10.4.2')
+    .option('-b, --browser [browser]'           , 'The target device\'s browser — i.e. : Mobile Safari, Chrome ...')
+    .option('--browserVersion [browserversion]' , 'The target device\'s browser\'s version — e.g. : 10.4.2')
+    .parse(process.argv);
 
 // existsSync moved from "path" to "fs" in Node.js v0.8,
 // keeping the fallback so that the code remains compatible with v0.6
 var existsSync = fs.existsSync || path.existsSync;
+/**
+ * The vars that will receive the args either way
+ */
+var adapter;
+var mainfile;
+var traceLevels = [];
+var minify = true;
+var useragent;
+var device;
+var formfactor;
+var system;
+var systemVersion;
+var browser;
+var browserVersion;
+
+/**
+ * Other vars used through the script
+ */
+var defaultUseragent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.71 Safari/537.36';
+var shouldUseCustomParsedUA = false;
+var customParsedUA = {
+  device      : { },
+  formfactor  : { },
+  os          : { },
+  ua          : { }
+};
 
 /**
  * Displays info about tool usage to the console
@@ -62,21 +104,59 @@ if (process.argv.length <= 2) {
   process.exit(1);
 }
 
-// Initialize the optimization parameters
-var adapter = process.argv[2];
-var mainfile = process.argv[3];
-var minify = true;
-if (process.argv[4]) {
-  minify = (process.argv[4] === 'false') ? false : true;
+// This is a retro-compatibility test. If we have sufficient number of params but that there is no
+// adapter nor a mainfile (both of which should be required), then we probably are in an old script.
+if (process.argv.length > 2 && !args.adapter && !args.mainfile) {
+  adapter = process.argv[2];
+  mainfile = process.argv[3];
+  if (process.argv[4]) {
+    minify = (process.argv[4] === 'false') ? false : true;
+  }
+  if (process.argv[5]) {
+    traceLevels = process.argv[5].split(',');
+  }
+  if (process.argv[6]) {
+    useragent = process.argv[6] || defaultUseragent;
+  }
+
+} else {
+
+  adapter             = args.adapter;   // These two are the minimum requirement.
+  mainfile            = args.input;     // Can't set them as required yet because compatibility.
+  minify              = args.minify ? args.minify : minify;
+  traceLevels         = args.woodman ? args.woodman.split(',') : traceLevels;
+  useragent           = args.useragent ? args.useragent : useragent;
+  device              = args.device;
+  formfactor          = args.formfactor;
+  system              = args.system;
+  browser             = args.browser;
+
+  if (args.systemVersion) { systemVersion = args.systemVersion.split('.'); }
+  if (args.browserVersion) { browserVersion = args.browserVersion.split('.'); }
+  if (device || formfactor || system || browser || (systemVersion && systemVersion.length) || (browserVersion && browserVersion.length)) {
+
+    customParsedUA.device.family      = device;
+    customParsedUA.formfactor.family  = formfactor;
+    customParsedUA.os.family          = system;
+    customParsedUA.os.patchMinor      = null;
+    customParsedUA.ua.family          = browser;
+
+    if (systemVersion && systemVersion.length) {
+      customParsedUA.os.major           = systemVersion[0];
+      customParsedUA.os.minor           = systemVersion[1];
+      customParsedUA.os.patch           = systemVersion[2];
+    }
+    if (browserVersion && browserVersion.length) {
+      customParsedUA.ua.major           = browserVersion[0];
+      customParsedUA.ua.minor           = browserVersion[1];
+      customParsedUA.ua.patch           = browserVersion[2];
+    }
+
+    shouldUseCustomParsedUA = true;
+  }
 }
-var traceLevels = [];
-if (process.argv[5]) {
-  traceLevels = process.argv[5].split(',');
-}
-var useragent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.71 Safari/537.36';
-if (process.argv[6]) {
-  useragent = process.argv[6];
-}
+
+
 
 var libPath = path.join(__dirname, '../lib');
 var js_almond = fs.readFileSync(libPath + '/vendor/almond.min.js', 'utf-8');
@@ -122,9 +202,12 @@ fs.writeFileSync('joshlib.js',
 * It will load files according to the present fixed configuration.
 **/
 var runtimeFile = 'runtime.js';
+var parsedUA = shouldUseCustomParsedUA ? customParsedUA : UAParser.parse(useragent);
+
 console.log('writing runtime file "' + runtimeFile + '"...');
-fs.writeFileSync(runtimeFile, 'define(' + JSON.stringify(UAParser.parse(useragent)) + ')', 'utf-8');
+fs.writeFileSync(runtimeFile, 'define(' + JSON.stringify(parsedUA) + ')', 'utf-8');
 console.log('writing runtime file "' + runtimeFile + '"... done');
+
 /**
 * END CREATE THE RUNTIME FILE
 **/
@@ -134,7 +217,15 @@ var adapterName = (adapter === 'none' ? '' : '.' + adapter);
 // Run require.js optimizer
 // Note the use of "mainConfigFile" option to have require.js read
 // the config from the main file.
-console.log('running require.js optimizer...');
+console.log('node ' + libPath + '/vendor/require.r.js -o' +
+  ' out=' + mainfile + adapterName + '.optimized.js' +
+  ' name=' + mainfile +
+  ' mainConfigFile=' + mainfile + '.js' +
+  ' paths.runtime=' + runtimeFile.split('.')[0] +
+  ' paths.devicedetect=' + libPath + '/devicedetect' +
+  ' baseUrl=.' +
+  ' optimize=none');
+
 exec('node ' + libPath + '/vendor/require.r.js -o' +
   ' out=' + mainfile + adapterName + '.optimized.js' +
   ' name=' + mainfile +
@@ -185,7 +276,7 @@ exec('node ' + libPath + '/vendor/require.r.js -o' +
     if (traceLevels.length > 0) {
       precompilerOptions.keepLevel = traceLevels;
     }
-    //optimized = woodmanPrecompile(optimized, precompilerOptions);
+    optimized = woodmanPrecompile(optimized, precompilerOptions);
     console.log('removing references to Woodman... done');
 
     // Minify the resulting code unless told not to
